99f4b617d58f5afffe68ee501ccdd63baf9950a8
damitzpinheiro <pinheiro.luciano@ime.eb.br>
2024-10-16 12:54:10 -0300

    Adição da classe Checksumnova

--- main.cpp
+++ main.cpp
@@ -29,13 +29,35 @@ class Checksum {
     }
 };
 
+class Checksumnova : public Checksum {
+   public:
+      virtual unsigned int novaChecksum(std::string message) {
+      if (message.size() % 2 != 0) {
+         message += '\0';
+      }
+
+      
+      unsigned int sum = 0;
+      
+      for (size_t i = 0; i < message.size(); i += 2) {
+         unsigned short word = (message[i] << 4) + (message[i + 1] & 0x0F);
+         sum += word;
+      }
+      return sum & 0xFFFF; 
+    }
+   }
+
 int main() {
     
-    Checksum checksum;
+   Checksum checksum;
+   Checksumnova checksumnova;
    std::string str = "Hello, World!";
    printf("Checksum of '%s' is %u\n", str.c_str(), checksum.calcChecksum(str));
-   
+   printf("Checksum of '%s' is %u\n", str.c_str(), checksumnova.novaChecksum(str));  
+
+
    std::vector<unsigned char> vec = {'H','e','l','l','o'};
-    printf("Checksum is %u\n", checksum.calcChecksum(vec));
+   printf("Checksum is %u\n", checksum.calcChecksum(vec));
+   printf("Checksum is %u\n", checksumnova.novaChecksum("Hello"));
    return 0;
 }
