8acfef7b20d4faec57f71c5bccc76a18102eb690
fmce520 <nicolas.fmce@imbel.gov.br>
2024-10-16 11:10:35 -0300

    main.cpp: métodos agora virtuais para permitir a sobrescrição na herança

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 class Checksum {
     public:
-    static unsigned int calcChecksum(std::string message) {
+    virtual unsigned int calcChecksum(std::string message) {
        unsigned int sum = 0;
        for(int i = 0; i < message.size(); i++) {
           sum += message[i];
@@ -12,11 +12,11 @@ class Checksum {
        return sum;
     }
     
-    static bool doChecksum(std::string message, unsigned int checksumValue){
+    virtual bool doChecksum(std::string message, unsigned int checksumValue){
         return (checksumValue == calcChecksum(message));
     }
     
-    static unsigned int calcChecksum(std::vector<unsigned char> message) {
+    virtual unsigned int calcChecksum(std::vector<unsigned char> message) {
        unsigned int sum = 0;
        for(int i = 0; i < message.size(); i++) {
           sum += message[i];
@@ -24,17 +24,18 @@ class Checksum {
        return sum;
     }
     
-    static bool doChecksum(std::vector<unsigned char> message, unsigned int checksumValue){
+    virtual bool doChecksum(std::vector<unsigned char> message, unsigned int checksumValue){
         return (checksumValue == calcChecksum(message));
     }
 };
 
 int main() {
     
+    Checksum checksum;
    std::string str = "Hello, World!";
-   printf("Checksum of '%s' is %u\n", str.c_str(), Checksum::calcChecksum(str));
+   printf("Checksum of '%s' is %u\n", str.c_str(), checksum.calcChecksum(str));
    
    std::vector<unsigned char> vec = {'H','e','l','l','o'};
-    printf("Checksum is %u\n", Checksum::calcChecksum(vec));
+    printf("Checksum is %u\n", checksum.calcChecksum(vec));
    return 0;
 }
