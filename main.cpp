1d2f12b0c1cd209a011ff6a121272c5d683b417d
fmce520 <nicolas.fmce@imbel.gov.br>
2024-10-23 12:08:33 -0300

    main.cpp: criação da classe de checksum usando XOR

--- main.cpp
+++ main.cpp
@@ -18,11 +18,34 @@ public:
     }
 };
 
+class ChecksumXOR : public Checksum {
+public:
+    virtual unsigned short calcChecksum(std::vector<unsigned char> message) {
+        unsigned char resultadoXor = 0;
+        for (unsigned int i = 0; i < message.size(); i++) {
+            if (i == 0) {
+                resultadoXor = message[i];
+            }
+            else {
+                resultadoXor = resultadoXor ^ message[i];
+            }
+        }
+        return resultadoXor;
+    }
+};
+
+//i=0 resultadoXor = message[0];
+//i=1 resultadoXor = message[0] ^ message[1];
+//i=2 resultadoXor = resultadoXor ^ message[2];
+
 int main() {
 
     Checksum checksum;
     std::vector<unsigned char> vec = { 'H','e','l','l','o' };
     printf("Checksum is %u\n", checksum.calcChecksum(vec));
+
+    ChecksumXOR checksumXOR;
+    printf("Checksum is %u\n", checksumXOR.calcChecksum(vec));
     return 0;
 
 }
