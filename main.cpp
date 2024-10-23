0dfc44dcb030bb1de2b660fb86e6e2800e201f4b
Raquel <raquelbelchior1@hotmail.com>
2024-10-23 12:08:32 -0300

    modificaçao do arquivo pro resultado ser o byte resultante

--- main.cpp
+++ main.cpp
@@ -18,10 +18,27 @@ public:
     }
 };
 
+class CustomChecksumXOR: public Checksum {
+public:
+    virtual unsigned short calcChecksum(std::vector<unsigned char> message) {
+        unsigned char resultadoxor = 0;
+        for(unsigned int i=0; i<message.size(); i++){
+            if(i==0){
+                resultadoxor=message[i];
+            }else{
+                resultadoxor=resultadoxor^message[i];
+            } 
+                   }
+        return resultadoxor;
+       }
+};
+
 int main() {
-    CustomChecksum customChecksum; // Usa a nova classe CustomChecksum
+    Checksum checksum; // Usa a nova classe CustomChecksum
     std::vector<unsigned char> vec = {'H', 'e', 'l', 'l', 'o'};
-    printf("Checksum is %u\n", customChecksum.calcChecksum(vec));
+    printf("Checksum is %u\n", checksum.calcChecksum(vec));
     
+    CustomChecksumXOR Customchecksum; // Usa a nova classe CustomChecksum
+    printf("Checksum is %u\n", Customchecksum.calcChecksum(vec));
     return 0;
 }
