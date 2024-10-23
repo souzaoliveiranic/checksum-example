864f314375a40a1720480954eb4a48866f5f8e1f
Frese <enzo.frese@hardwario.com>
2024-10-23 11:43:00 -0300

    main: Rebase

--- main.cpp
+++ main.cpp
@@ -2,31 +2,18 @@
 #include <vector>
 #include <string>
 
-using namespace std;
-
 class Checksum {
-    public:
-    virtual unsigned int calcChecksum(string message) {
-       unsigned int sum = 0;
-       for(int i = 0; i < message.size(); i++) {
-          sum += message[i];
-       } 
-       return sum;
+
+public:
+    virtual unsigned short calcChecksum(std::vector<unsigned char> message) {
+        unsigned short sum = 0;
+        for (unsigned int i = 0; i < message.size(); i++) {
+            sum += message[i];
+        }
+        return sum;
     }
-    
-    virtual bool doChecksum(string message, unsigned int checksumValue){
-        return (checksumValue == calcChecksum(message));
-    }
-    
-    virtual unsigned int calcChecksum(vector<unsigned char> message) {
-       unsigned int sum = 0;
-       for(int i = 0; i < message.size(); i++) {
-          sum += message[i];
-       } 
-       return sum;
-    }
-    
-    virtual bool doChecksum(vector<unsigned char> message, unsigned int checksumValue){
+
+    virtual bool doChecksum(std::vector<unsigned char> message, unsigned short checksumValue) {
         return (checksumValue == calcChecksum(message));
     }
 };
@@ -34,10 +21,7 @@ class Checksum {
 int main() {
 
     Checksum checksum;
-    string str = "Hello, World!";
-    printf("Checksum of '%s' is %u\n", str.c_str(), checksum.calcChecksum(str));
-   
-    vector<unsigned char> vec = {'H','e','l','l','o'};
+    std::vector<unsigned char> vec = { 'H','e','l','l','o' };
     printf("Checksum is %u\n", checksum.calcChecksum(vec));
     return 0;
 
