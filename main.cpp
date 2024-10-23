38313acf33c51bb187c589bed8c77b4faf26010b
unknown <sibuya.douglas@ime.eb.br>
2024-10-23 12:53:40 -0300

    main.cpp: item 2

--- main.cpp
+++ main.cpp
@@ -16,6 +16,16 @@ public:
     virtual bool doChecksum(std::vector<unsigned char> message, unsigned short checksumValue) {
         return (checksumValue == calcChecksum(message));
     }
+
+    // Novo método para adicionar checksum ao final da mensagem
+    virtual void appendChecksum(std::vector<unsigned char>& message, unsigned short checksumValue, unsigned int checksumSize) {
+        for (unsigned int i = 0; i < checksumSize; i++) {
+            unsigned char byte = (checksumValue >> (8 * (checksumSize - 1 - i))) & 0xFF;
+            message.push_back(byte);
+        }
+    }
+    
+
 };
 
 
@@ -45,15 +55,26 @@ int main() {
 
     Checksum checksum;
     std::vector<unsigned char> vec = { 'H','e','l','l','o' };
-    printf("Checksum is %u\n", checksum.calcChecksum(vec));
 
+    printf("Checksum (soma) is %u\n", checksum.calcChecksum(vec));
 
 
     ChecksumXOR checksumXOR;
-    printf("Checksum is %u\n", checksumXOR.calcChecksum(vec));
+    printf("Checksum (XOR) is %u\n", checksumXOR.calcChecksum(vec));
+    
+
+    // Adiciona checksum ao final da mensagem
+    checksum.appendChecksum(vec, checksum.calcChecksum(vec), 2); // Exemplo: adiciona 2 bytes de checksum
+
+    // Exibe mensagem com checksum anexado
+    printf("Mensagem com checksum: ");
+    for (unsigned char c : vec) {
+        printf("%02X ", c);
+    }
+    printf("\n");
+
+
     return 0;
-
-
 }
 
 
