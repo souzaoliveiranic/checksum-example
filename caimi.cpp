bbbdaf7875cb3d667ef1469c73e71b15f17faab6
guiCaimi <caimi.guilherme@ime.eb.br>
2024-10-23 12:08:46 -0300

    criaçao da nova classe

--- caimi.cpp
+++ caimi.cpp
@@ -5,7 +5,7 @@
 class Checksum
 {
 public:
-   virtual unsigned int calcChecksum(std::vector<unsigned char> message)
+   virtual unsigned short calcChecksum(std::vector<unsigned char> message)
    {
       unsigned int sum = 0;
       for (int i = 0; i < message.size(); i++)
@@ -21,12 +21,38 @@ public:
    }
 };
 
+class ChecksumXOR : public Checksum
+{
+public:
+   // Sobrescreve o método de cálculo do checksum usando XOR
+   unsigned short calcChecksum(std::vector<unsigned char> message) override
+   {
+      unsigned char resultadoXOR = 0;
+      for (unsigned int i = 0; i < message.size(); i++)
+      {
+         if (i == 0)
+         {
+            resultadoXOR = message[0];
+         }
+         else
+         {
+            resultadoXOR = resultadoXOR ^ message[i];
+         }
+      }
+      return resultadoXOR;
+   }
+};
+
 int main()
 {
-
    Checksum checksum;
 
-   std::vector<unsigned char> vec = {'H', 'e', 'l', 'l', 'o'};
-   printf("Checksum is %u\n", checksum.calcChecksum(vec));
+   std::vector<unsigned char> vec = {'H', 'E', 'L', 'L', 'O'};
+   printf("Checksum (soma) is %u\n", checksum.calcChecksum(vec));
+
+   ChecksumXOR checksumXOR;
+
+   printf("Checksum (XOR) is %u\n", checksumXOR.calcChecksum(vec));
+
    return 0;
 }
