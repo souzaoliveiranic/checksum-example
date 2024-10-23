e74030e3d8bc21063dada7aa088fe55d32827b92
Robsonnrj <robson.junior@ime.eb.br>
2024-10-23 12:13:05 -0300

    main.cpp: criação da classe de checksum usando XOR

--- main.cpp
+++ main.cpp
@@ -5,25 +5,41 @@
 // Teste teste
 
 class Checksum {
-   
-    virtual unsigned int calcChecksum(std::vector<unsigned char> message) {
-       unsigned int sum = 0;
-       for(int i = 0; i < message.size(); i++) {
-          sum += message[i];
-       } 
-       return sum;
+public:
+    virtual unsigned short calcChecksum(std::vector<unsigned char> message) {
+        unsigned int sum = 0;
+        for (int i = 0; i < message.size(); i++) {
+            sum += message[i];
+        }
+        return sum;
     }
-    
-    virtual bool doChecksum(std::vector<unsigned char> message, unsigned int checksumValue){
+
+    virtual bool doChecksum(std::vector<unsigned char> message, unsigned int checksumValue) {
         return (checksumValue == calcChecksum(message));
+    }
+};
+class ChecksumXOR : public Checksum {
+public:
+    virtual unsigned short calcChecksum(std::vector<unsigned char> message) {
+        unsigned char resultadoXOR = 0;
+        for (unsigned int i = 0; i < message.size(); i++) {
+            if (i == 0) {
+                resultadoXOR = message[i];
+            }
+            else {
+                resultadoXOR = resultadoXOR ^ message[i];
+            }
+
+        }
+        return resultadoXOR;
     }
 };
 
 int main() {
-    
+
     Checksum checksum;
-   
-   std::vector<unsigned char> vec = {'H','e','l','l','o'};
+
+    std::vector<unsigned char> vec = { 'H','e','l','l','o' };
     printf("Checksum is %u\n", checksum.calcChecksum(vec));
-   return 0;
+    return 0;
 }
