4f24995510cd8ed356c0ef8361d3b17ce11f81ae
fmce520 <nicolas.fmce@imbel.gov.br>
2024-10-22 21:01:40 -0300

    simplificação da classe checksum e mudança do type do checksum

--- main.cpp
+++ main.cpp
@@ -4,27 +4,15 @@
 
 class Checksum {
     public:
-    virtual unsigned int calcChecksum(std::string message) {
-       unsigned int sum = 0;
-       for(int i = 0; i < message.size(); i++) {
+    virtual unsigned short calcChecksum(std::string message) {
+       unsigned short sum = 0;
+       for(unsigned int i = 0; i < message.size(); i++) {
           sum += message[i];
        } 
        return sum;
     }
     
-    virtual bool doChecksum(std::string message, unsigned int checksumValue){
-        return (checksumValue == calcChecksum(message));
-    }
-    
-    virtual unsigned int calcChecksum(std::vector<unsigned char> message) {
-       unsigned int sum = 0;
-       for(int i = 0; i < message.size(); i++) {
-          sum += message[i];
-       } 
-       return sum;
-    }
-    
-    virtual bool doChecksum(std::vector<unsigned char> message, unsigned int checksumValue){
+    virtual bool doChecksum(std::string message, unsigned short checksumValue){
         return (checksumValue == calcChecksum(message));
     }
 };
@@ -32,12 +20,9 @@ class Checksum {
 int main() {
     
     Checksum checksum;
-   std::string str = "Hello, World!";
-   printf("Checksum of '%s' is %u\n", str.c_str(), checksum.calcChecksum(str));
+	std::string str = "Hello, World!";
+	printf("Checksum of '%s' is %u\n", str.c_str(), checksum.calcChecksum(str));
    
-   std::vector<unsigned char> vec = {'H','e','l','l','o'};
-    printf("Checksum is %u\n", checksum.calcChecksum(vec));
-   return 0;
+	return 0;
 }
 
-///Rabelo irá commitar.
