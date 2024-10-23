6b38bdc1b3f3a9299a6161825b64d4b8a18fb7bc
Eduardo-Peixoto <eduardo.peixoto1003@gmail.com>
2024-10-23 12:49:38 -0300

    mensagem com o checksum

--- main.cpp
+++ main.cpp
@@ -15,9 +15,14 @@ class Checksum {
     virtual bool doChecksum(std::vector<unsigned char> message, unsigned int checksumValue){
         return (checksumValue == calcChecksum(message));
     }
+
+    virtual std::vector<unsigned char> adicionar_checksum(std::vector<unsigned char> message, unsigned short calcChecksum) {
+        message.push_back(calcChecksum);
+        return message;
+    }
 };
 
-class ChecksumXOR: public Checksum{
+class ChecksumXOR: public Checksum {
    public:
    virtual unsigned short calcChecksum(std::vector<unsigned char> message) {
        unsigned short resultadoXor = 0;
@@ -41,5 +46,11 @@ int main() {
 
    ChecksumXOR checksumXOR;
    printf("Checksum is %u\n", checksumXOR.calcChecksum(vec));
+
+   std::vector<unsigned char> vetor_com_checksum = checksum.adicionar_checksum(vec, checksum.calcChecksum(vec));
+    printf("Message with sum checksum: ");
+    for (int i = 0; i < vetor_com_checksum.size(); i++) {
+        printf("%c", vetor_com_checksum[i]);
+    }
    return 0;
 }
