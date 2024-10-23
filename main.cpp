db3144e5d17fed50fc972d25807e4cad6ecca7d1
unknown <sibuya.douglas@ime.eb.br>
2024-10-23 12:13:31 -0300

    main.cpp: criação da classe ChecksumXOR

--- main.cpp
+++ main.cpp
@@ -18,12 +18,44 @@ public:
     }
 };
 
+
+class ChecksumXOR : public Checksum {
+    public:
+    virtual unsigned short calcChecksum(std::vector<unsigned char> message) {
+        unsigned char resultadoXor = 0;
+        for(unsigned int i = 0; i < message.size(); i++) {
+            if (i == 0){
+                resultadoXor = message[i];
+            }
+            else{
+                resultadoXor = resultadoXor ^ message[i];
+            }
+
+        }
+        return resultadoXor;
+    }
+
+
+};
+
+
+
+
 int main() {
 
     Checksum checksum;
     std::vector<unsigned char> vec = { 'H','e','l','l','o' };
     printf("Checksum is %u\n", checksum.calcChecksum(vec));
+
+
+
+    ChecksumXOR checksumXOR;
+    printf("Checksum is %u\n", checksumXOR.calcChecksum(vec));
     return 0;
 
+
 }
+
+
+
 
