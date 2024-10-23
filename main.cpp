1ffc0e0b0f296c6492a6c5e79585d55206b59bca
LeoKeiji <leo.keiji@gmail.com>
2024-10-23 12:46:16 -0300

    PEE: Added list 5 exercises 1-2

--- main.cpp
+++ main.cpp
@@ -4,62 +4,69 @@
 
 class Checksum {
 public:
-    virtual unsigned int calcChecksum(std::string message) {
-        unsigned int sum = 0;
-        for(unsigned int i = 0; i < message.size(); i++) {
+    virtual unsigned short calcChecksum(std::vector<unsigned char> message) {
+        unsigned short sum = 0;
+        for (unsigned int i = 0; i < message.size(); i++) {
             sum += message[i];
-        } 
+        }
         return sum;
     }
 
-    virtual bool doChecksum(std::string message, unsigned int checksumValue) {
+    virtual bool doChecksum(std::vector<unsigned char> message, unsigned short checksumValue) {
         return (checksumValue == calcChecksum(message));
     }
 
-    virtual unsigned int calcChecksum(std::vector<unsigned char> message) {
-        unsigned int sum = 0;
-        for(unsigned int i = 0; i < message.size(); i++) {
-            sum += message[i];
-        } 
-        return sum;
-    }
-
-    virtual bool doChecksum(std::vector<unsigned char> message, unsigned int checksumValue) {
-        return (checksumValue == calcChecksum(message));
+    std::vector<unsigned char> appendChecksum(std::vector<unsigned char> message, size_t checksumBytes) {
+        unsigned short checksum = calcChecksum(message);
+        std::vector<unsigned char> checksumVector(checksumBytes, 0);
+        for (unsigned int i = 0; i < checksumBytes; i++) {
+            checksumVector[checksumBytes - 1 - i] = (checksum >> (8 * i)) & 0xFF;
+        }
+        message.insert(message.end(), checksumVector.begin(), checksumVector.end());
+        return message;
     }
 };
 
-class MyChecksum : public Checksum {
+class ChecksumXOR : public Checksum {
 public:
-    unsigned int calcChecksum(std::vector<unsigned char> message) override {
-        unsigned int sum = 0;
-
-        if (message.size() % 2 != 0) {
-            message.push_back(0x00);  
+    virtual unsigned short calcChecksum(std::string message) {
+        unsigned char resultadoXor = 0;
+        for (unsigned int i = 0; i < message.size(); i++) {
+            resultadoXor ^= message[i];
         }
-
-        for (size_t i = 0; i < message.size(); i += 2) {
-            unsigned short palavra = (message[i] << 8) | message[i + 1]; 
-            sum += palavra;
-        }
-
-        return sum & 0xFFFF; 
+        return resultadoXor;
     }
-    
-    unsigned int calcChecksum(std::string message) override {
-        std::vector<unsigned char> byteMessage(message.begin(), message.end());
-        return calcChecksum(byteMessage); 
+
+    std::string appendChecksum(std::string message, size_t checksumBytes) {
+        unsigned short checksum = calcChecksum(message);
+        std::string checksumStr(checksumBytes, 0);
+        for (unsigned int i = 0; i < checksumBytes; i++) {
+            checksumStr[checksumBytes - 1 - i] = (checksum >> (8 * i)) & 0xFF;
+        }
+        return message + checksumStr;
     }
 };
 
 int main() {
-    MyChecksum checksum;
+    Checksum checksum;
+    std::vector<unsigned char> vec = { 'H', 'e', 'l', 'l', 'o' };
+    std::vector<unsigned char> resultSum = checksum.appendChecksum(vec, 3);
 
-    std::string str = "Hello, World!";
-    printf("Checksum de '%s' é %u\n", str.c_str(), checksum.calcChecksum(str));
+    ChecksumXOR checksumXor;
+    std::string message = "Hello";
+    std::string resultXor = checksumXor.appendChecksum(message, 3);
 
-    std::vector<unsigned char> vec = {'H', 'e', 'l', 'l', 'o'};
-    printf("Checksum do vetor é %u\n", checksum.calcChecksum(vec));
+    printf("Mensagem com checksum (SUM): ");
+    for (unsigned char byte : resultSum) {
+        printf("%d ", byte);
+    }
+    printf("\n");
+
+    printf("Mensagem com checksum (XOR): ");
+    for (unsigned char byte : resultXor) {
+        printf("%d ", byte);
+    }
+    printf("\n");
 
     return 0;
 }
